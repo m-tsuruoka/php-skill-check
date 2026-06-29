0944d709bb3a14783a83a610920eb7ec64f70934
m.tsuruoka <m.tsuruoka@core-tech.jp>
2026-06-29 10:44:06 +0900

    登録フォームの作成

--- src/Views/books/create.php
+++ src/Views/books/create.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * ★基礎課題: 新規登録フォームのビュー
  *
@@ -15,9 +16,65 @@
  *   - エラーがあれば <p class="error"> で表示する
  */
 $title = '新規登録';
+
+/** @var array $categories */
+/** @var array $errors */
+/** @var array $old */
+
+$errors = $errors ?? [];
+$old = $old ?? [];
+
 ?>
 
 <h2>新規書籍登録（このページを実装してください）</h2>
 
 <p class="muted">README の「基礎課題」に従って登録フォームを作成してください。</p>
 <p><a class="btn" href="/">← 一覧へ戻る</a></p>
+
+<form method="post" action="/?page=store">
+    <div>
+        <label for="title">タイトル</label>
+        <input type="text" id="title" name="title" value="<?= e($old['title'] ?? '') ?>">
+        <?php if (!empty($errors['title'])): ?>
+            <p class="error" style="color: red;"><?= e($errors['title']) ?></p>
+        <?php endif; ?>
+    </div>
+
+    <div>
+        <label for="author">著者</label>
+        <input type="text" id="author" name="author" value="<?= e($old['author'] ?? '') ?>">
+        <?php if (!empty($errors['author'])): ?>
+            <p class="error" style="color: red;"><?= e($errors['author']) ?></p>
+        <?php endif; ?>
+    </div>
+
+    <div>
+        <label for="category_id">カテゴリ</label>
+        <select id="category_id" name="category_id">
+            <option value="">-- カテゴリを選択してください --</option>
+            <?php foreach ($categories as $category): ?>
+                <?php
+                $selected = (isset($old['category_id']) && $old['category_id'] == $category['id'])
+                    ? 'selected'
+                    : '';
+                ?>
+                <option value="<?= e($category['id']) ?>" <?= $selected ?>>
+                    <?= e($category['name']) ?>
+                </option>
+            <?php endforeach; ?>
+        </select>
+        <?php if (!empty($errors['category_id'])): ?>
+            <p class="error" style="color: red;"><?= e($errors['category_id']) ?></p>
+        <?php endif; ?>
+    </div>
+
+    <div>
+        <label for="price">価格</label>
+        <input type="number" id="price" name="price" value="<?= e($old['price'] ?? '') ?>">
+        <?php if (!empty($errors['price'])): ?>
+            <p class="error" style="color: red;"><?= e($errors['price']) ?></p>
+        <?php endif; ?>
+    </div>
+
+    <button type="submit" class="btn">登録</button>
+</form>
